e16daec1524d0fdf749691561799c0756b9149f4
mtr65 <mtr@mattrobbins.net>
2020-05-29 23:16:46 -0500

    Added source file/directory

---
+++ src/tempidity-logger.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# Source File name: humidity_logger_v5.py
+
+import os
+import time
+import datetime
+import Adafruit_DHT
+
+DHT_SENSOR = Adafruit_DHT.DHT22
+DHT_PIN = 4
+
+try:
+    fo = open("/home/wisdom/Code/Python/tempidity_data.csv", "a+")
+    if os.stat("/home/wisdom/Code/Python/tempidity_data.csv").st_size == 0:
+        fo.write("Date,Time,Temperature,Humidity\r\n")
+
+except:
+    pass
+
+while True:
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    now = datetime.datetime.now()
+    tempfahr = (temperature * 1.8 ) + 32
+                                                    
+    if humidity is not None and temperature is not None:
+        fo = open("/home/wisdom/Code/Python/tempidity_data.csv", "a+")
+        fo.write('{0},{1},{2:0.1f},{3:0.1f}\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M:%S'), temperature, humidity))
+#         print (now.strftime("%Y-%m-%d %H:%M:%S"))
+        print(now.strftime("%m-%d-%Y %H:%M:%S"),"Temp: {0:0.1f}\u00B0C {1:0.1f}\u00B0F Humidity: {2:0.1f}%".format(temperature, tempfahr, humidity))
+        fo.close()
+    else:
+        print ("Failed to retrieve data from sensor")
+
+    time.sleep(30)
