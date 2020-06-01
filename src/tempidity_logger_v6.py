4d4dc659a7fed781f52b7af9e57999779f54e2ce
mtr65 <mtr@mattrobbins.net>
2020-06-01 15:46:51 -0500

    Removed backup file

--- src/tempidity_logger_v6.py
+++ src/tempidity_logger_v6.py
@@ -18,9 +18,11 @@ CYELLOWBG  = '\33[43m'
 CGREENBG   = '\33[42m'
 CRED2      = '\33[91m'
 
+LOGDIR     = '~/log'
+
 try:
-    fo = open("/home/wisdom/Code/Python/tempidity_data.csv", "a+")
-    if os.stat("/home/wisdom/Code/Python/tempidity_data.csv").st_size == 0:
+    fo = open("~/log/tempidity_data.csv", "a+")
+    if os.stat("~/log/tempidity_data.csv").st_size == 0:
         fo.write("Date,Time,Temperature,Humidity\r\n")
 
     os.system('clear')    
@@ -35,7 +37,7 @@ while True:
     tempfahr = (temperature * 1.8 ) + 32
                                                     
     if humidity is not None and temperature is not None:
-        fo = open("/home/wisdom/Code/Python/tempidity_data.csv", "a+")
+        fo = open("~/log/tempidity_data.csv", "a+")
         fo.write('{0},{1},{2:0.1f},{3:0.1f}\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M:%S'), temperature, humidity))
 #         print (now.strftime("%Y-%m-%d %H:%M:%S"))
         print(now.strftime(CRED2 + " %m-%d-%Y %H:%M:%S" + CEND), CBLUEBG + " Temp: {0:0.1f}\u00B0C/{1:0.1f}\u00B0F \33[0m" " \33[42m Humidity: {2:0.1f}% \33[0m" .format(temperature, tempfahr, humidity))
