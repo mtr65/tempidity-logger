e387b2706866555457af9aad620f0c84f514de08
mtr65 <mtr@mattrobbins.net>
2020-05-31 22:27:44 -0500

    Read CHANGELOG.md for update info

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+	05-31-2020: v6 added color text, new formatting and a banner for the terminal display.
+	
--- src/tempidity_logger_v6.py
+++ src/tempidity_logger_v6.py
@@ -10,12 +10,20 @@ import datetime
 import Adafruit_DHT
 
 DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 4
+DHT_PIN    = 4
+
+CEND       = '\33[0m'
+CBLUEBG    = '\33[44m'
+CYELLOWBG  = '\33[43m'
+CRED2      = '\33[91m'
 
 try:
     fo = open("/home/wisdom/Code/Python/tempidity_data.csv", "a+")
     if os.stat("/home/wisdom/Code/Python/tempidity_data.csv").st_size == 0:
         fo.write("Date,Time,Temperature,Humidity\r\n")
+
+    os.system('clear')    
+    print("\n\n  \33[7m** Reading temperature and humidity data from Sensor#1 ** \33[0m\n\n")    
 
 except:
     pass
@@ -29,7 +37,7 @@ while True:
         fo = open("/home/wisdom/Code/Python/tempidity_data.csv", "a+")
         fo.write('{0},{1},{2:0.1f},{3:0.1f}\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M:%S'), temperature, humidity))
 #         print (now.strftime("%Y-%m-%d %H:%M:%S"))
-        print(now.strftime("%m-%d-%Y %H:%M:%S"),"Temp: {0:0.1f}\u00B0C/{1:0.1f}\u00B0F Humidity: {2:0.1f}%".format(temperature, tempfahr, humidity))
+        print(now.strftime(CRED2 + " %m-%d-%Y %H:%M:%S" + CEND), CBLUEBG + " Temp: {0:0.1f}\u00B0C/{1:0.1f}\u00B0F \33[0m " "\33[42m Humidity: {2:0.1f}% \33[0m".format(temperature, tempfahr, humidity))
         fo.close()
     else:
         print ("Failed to retrieve data from sensor")
