9c49324510a7dc94a94984627a0012619e1d9866
mtr65 <mtr@mattrobbins.net>
2020-06-09 00:42:15 -0500

    Added path variable for log file

--- src/tempidity_logger_v6.1.py
+++ src/tempidity_logger_v6.1.py
@@ -8,6 +8,7 @@ import os
 import time
 import datetime
 import Adafruit_DHT
+#import constant
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN    = 4
@@ -17,13 +18,15 @@ CBLUEBG    = '\33[44m'
 CYELLOWBG  = '\33[43m'
 CRED2      = '\33[91m'
 
-try:
-    fo = open("/home/wisdom/Code/Python/tempidity_data.csv", "a+")
-    if os.stat("/home/wisdom/Code/Python/tempidity_data.csv").st_size == 0:
-        fo.write("Date,Time,Temperature,Humidity\r\n")
+homedir = os.environ['HOME']
 
+try:
     os.system('clear')    
-    print("\n\n  \33[7m Reading temperature and humidity data from  \33[92m Sensor #1 \33[0m\n\n")    
+    print("\n\n  \33[7m Reading temperature and humidity data from  \33[92m Sensor #1 \33[0m\n\n")
+    
+    fo = open("{homedir}/Code/Python/tempidity_data.csv", "a+")
+    if os.stat("{homedir}/Code/Python/tempidity_data.csv").st_size == 0:
+        fo.write("Date,Time,Temperature,Humidity\r\n")
 
 except:
     pass
@@ -32,9 +35,10 @@ while True:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
     now = datetime.datetime.now()
     tempfahr = (temperature * 1.8 ) + 32
-                                                    
+    
     if humidity is not None and temperature is not None:
         fo = open("/home/wisdom/Code/Python/tempidity_data.csv", "a+")
+#        fo = open("{homedir}/Code/Python/tempidity_data.csv", "a+")
         fo.write('{0},{1},{2:0.1f},{3:0.1f}\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M:%S'), temperature, humidity))
 #         print (now.strftime("%Y-%m-%d %H:%M:%S"))
         print(now.strftime(CRED2 + " %m-%d-%Y %H:%M:%S" + CEND), CBLUEBG + " Temp: {0:0.1f}\u00B0C/{1:0.1f}\u00B0F \33[0m " "\33[42m Humidity: {2:0.1f}% \33[0m".format(temperature, tempfahr, humidity))
