2de044eb3c30e4d3a61209eed164cd41bb91159e
Matt Robbins <mtr@mattrobbins.net>
2020-10-14 12:38:08 -0500

    Added new file tempidity-LCDlogger_betaV1.0.3.py.

---
+++ src/tempidity-LCDlogger_betaV1.0.3.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python3
+
+from subprocess import Popen, PIPE
+from time import sleep
+from datetime import datetime
+import board
+import digitalio
+import adafruit_character_lcd.character_lcd as characterlcd
+import Adafruit_DHT
+
+DHT_SENSOR = Adafruit_DHT.DHT22
+DHT_PIN    = 4
+
+# Modify this if you have a different sized character LCD
+lcd_columns = 16
+lcd_rows = 2
+
+# compatible with all versions of RPI as of Jan. 2019
+# v1 - v3B+
+lcd_rs = digitalio.DigitalInOut(board.D22)
+lcd_en = digitalio.DigitalInOut(board.D17)
+lcd_d4 = digitalio.DigitalInOut(board.D25)
+lcd_d5 = digitalio.DigitalInOut(board.D24)
+lcd_d6 = digitalio.DigitalInOut(board.D23)
+lcd_d7 = digitalio.DigitalInOut(board.D18)
+lcd_backlight = digitalio.DigitalInOut(board.D5)
+
+# Initialise the lcd class
+lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6,
+                                      lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+
+# Create applicable byte codes for a standard pre-programmed character
+# on a HD44780 LCD controller
+checkmark = bytes([0x0, 0x18, 0x18, 0x18, 0x0, 0x0, 0x0, 0x0])
+
+# Store byte code in LCD character memory 0
+lcd.create_char(0, checkmark)
+
+# lcd.message = "\x00"
+
+
+# looking for an active Ethernet or WiFi device
+def find_interface():
+    find_device = "ip addr show"
+    interface_parse = run_cmd(find_device)
+    for line in interface_parse.splitlines():
+        if "state UP" in line:
+            dev_name = line.split(':')[1]
+    return dev_name
+
+# find an active IP on the first LIVE network device
+def parse_ip():
+    find_ip = "ip addr show %s" % interface
+    find_ip = "ip addr show %s" % interface
+    ip_parse = run_cmd(find_ip)
+    for line in ip_parse.splitlines():
+        if "inet " in line:
+            ip = line.split(' ')[5]
+            ip = ip.split('/')[0]
+    return ip
+
+# run unix shell command, return as ASCII
+def run_cmd(cmd):
+    p = Popen(cmd, shell=True, stdout=PIPE)
+    output = p.communicate()[0]
+    return output.decode('ascii')
+
+# wipe LCD screen before we start
+# lcd.backlight = true
+lcd.clear()
+
+# before we start the main loop - detect active network device and ip address
+sleep(2)
+interface = find_interface()
+ip_address = parse_ip()
+
+while True:
+    # lcd.clear()
+    # Read DHT22 module data
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    tempfahr = (temperature * 1.8 ) + 32
+
+    # date and time
+    # lcd_line_1 = datetime.now().strftime('%b %d  %H:%M:%S\n')
+    # lcd.message = datetime.now().strftime('%b %d  %H:%M:%S\n')
+    # sleep(10)
+    # lcd.clear()
+    lcd_line_1 = ('Temp: %d\x00C ' % temperature)
+    lcd_line_2 = ('%d\x00F\n' % tempfahr)
+
+    # lcd_line_1 = ('Temp: {0:0.1f}C/{1:0.1f}F\n'.format(temperature, tempfahr)
+
+    # current ip address
+    # lcd_line_2 = "IP " + ip_address
+    lcd_line_3 = ('Humidity: %d%%' % humidity)
+    # lcd_line_2 = ('Humidity: {0}%%'.format(humidity)
+
+    # combine both lines into one update to the display
+    lcd.message = lcd_line_1 + lcd_line_2 + lcd_line_3
+
+    # scroll_msg = lcd_line_1 + lcd_line_2 + lcd_line_3
+    # scroll_msg = "Yo Dude!"
+    # lcd.message = scroll_msg
+    # Scroll message to the left
+    # for i in range(len(scroll_msg)):
+       # sleep(0.5)
+       # lcd.move_left()
+    # lcd.clear()
+
+    sleep(3)
