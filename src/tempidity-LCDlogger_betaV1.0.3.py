4efe5f91aa9f146b90dc2d8ac3c34c764fa612b5
Matt Robbins <mtr@mattrobbins.net>
2020-10-14 12:46:42 -0500

    Cleaning up unused code.

--- src/tempidity-LCDlogger_betaV1.0.3.py
+++ src/tempidity-LCDlogger_betaV1.0.3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-from subprocess import Popen, PIPE
+#from subprocess import Popen, PIPE
 from time import sleep
-from datetime import datetime
+#from datetime import datetime
 import board
 import digitalio
 import adafruit_character_lcd.character_lcd as characterlcd
@@ -40,30 +40,30 @@ lcd.create_char(0, checkmark)
 
 
 # looking for an active Ethernet or WiFi device
-def find_interface():
-    find_device = "ip addr show"
-    interface_parse = run_cmd(find_device)
-    for line in interface_parse.splitlines():
-        if "state UP" in line:
-            dev_name = line.split(':')[1]
-    return dev_name
+#def find_interface():
+#    find_device = "ip addr show"
+#    interface_parse = run_cmd(find_device)
+#    for line in interface_parse.splitlines():
+#        if "state UP" in line:
+#            dev_name = line.split(':')[1]
+#    return dev_name
 
 # find an active IP on the first LIVE network device
-def parse_ip():
-    find_ip = "ip addr show %s" % interface
-    find_ip = "ip addr show %s" % interface
-    ip_parse = run_cmd(find_ip)
-    for line in ip_parse.splitlines():
-        if "inet " in line:
-            ip = line.split(' ')[5]
-            ip = ip.split('/')[0]
-    return ip
+#def parse_ip():
+#    find_ip = "ip addr show %s" % interface
+#    find_ip = "ip addr show %s" % interface
+#    ip_parse = run_cmd(find_ip)
+#    for line in ip_parse.splitlines():
+#        if "inet " in line:
+#            ip = line.split(' ')[5]
+#            ip = ip.split('/')[0]
+#    return ip
 
 # run unix shell command, return as ASCII
-def run_cmd(cmd):
-    p = Popen(cmd, shell=True, stdout=PIPE)
-    output = p.communicate()[0]
-    return output.decode('ascii')
+#def run_cmd(cmd):
+#    p = Popen(cmd, shell=True, stdout=PIPE)
+#    output = p.communicate()[0]
+#    return output.decode('ascii')
 
 # wipe LCD screen before we start
 # lcd.backlight = true
@@ -71,8 +71,8 @@ lcd.clear()
 
 # before we start the main loop - detect active network device and ip address
 sleep(2)
-interface = find_interface()
-ip_address = parse_ip()
+#interface = find_interface()
+#ip_address = parse_ip()
 
 while True:
     # lcd.clear()
@@ -107,4 +107,4 @@ while True:
        # lcd.move_left()
     # lcd.clear()
 
-    sleep(3)
+    sleep(10)
